b48b24a9d55737f69a56bda462cc76c1601d2875
gabady13 <gabadysergei@yandex.ru>
2025-02-08 11:03:52 +0300

    test five

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'article/index.html', context={
+        'test': 'Yes, its working!',
+    })
+
 
 def index(request):
     return render(request, 'article/index.html', context={
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,5 +1,17 @@
 # hexlet_django_blog/views.py
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
+
 
 def index(request):
     return render(request, 'index.html', context={
