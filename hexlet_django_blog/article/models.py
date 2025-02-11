c90f7b95cb688a1c871da9fc929417f0b62af02e
gabady13 <gabadysergei@yandex.ru>
2025-02-11 20:07:45 +0300

    9

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Article(models.Model):
@@ -6,3 +7,6 @@ class Article(models.Model):
     body = models.TextField() # тело статьи
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
