5acab27040ca470833a82cac0b1df6a62aa09408
gabady13 <gabadysergei@yandex.ru>
2025-02-11 20:26:07 +0300

    10

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from hexlet_django_blog.article import views
 from hexlet_django_blog.article.views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('<str:tags>/<int:article_id>/', views.index, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
+from hexlet_django_blog.article.models import Article
+
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
         return render(request, 'article/index.html', context={
-        'test': 'Yes, its working!',
+        'articles': articles,
     })
 
 
